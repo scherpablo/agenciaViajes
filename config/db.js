b00f62d992a07456bdf5b3f66a162d18a5203fea
scherpablo <scherpablo@gmail.com>
2023-05-07 00:01:45 -0300

    quinto commit

--- config/db.js
+++ config/db.js
@@ -15,7 +15,8 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
         min: 0,
         acquire: 30000,
         idle: 10000
-    }, operatorsAliases: false
+    }, 
+    // operatorsAliases: false
 });
 
 export default db;
