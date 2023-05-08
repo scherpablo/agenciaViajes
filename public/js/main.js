047e5425130069490d3f88e5a1b32f417eaed2e8
scherpablo <scherpablo@gmail.com>
2023-05-08 01:06:47 -0300

    quinto commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,6 @@
 const themeToggle = document.querySelector('#theme-toggle');
 const html = document.querySelector('html');
-// const lightStyleLink = document.querySelector('link[href="/css/light-style.css"]');
 const lightStyleLink = document.querySelector('link[href$="light-style.css"]');
-// const darkStyleLink = document.querySelector('link[href="/css/dark-style.css"]');
 const darkStyleLink = document.querySelector('link[href$="dark-style.css"]');
 
 themeToggle.addEventListener('click', () => {
@@ -13,15 +11,14 @@ themeToggle.addEventListener('click', () => {
     // Verificar el nuevo valor del dataset para cambiar los estilos
     if (html.dataset.theme === 'light') {
         lightStyleLink.disabled = true;
-        darkStyleLink.disabled = false;  
-        
+        darkStyleLink.disabled = false;          
         themeToggle.style.backgroundColor = '#ffffff';
         themeToggle.textContent = 'LIGHT MODE';
     } else {
         lightStyleLink.disabled = false;
         darkStyleLink.disabled = true; 
-
         themeToggle.style.backgroundColor = '#444444';
         themeToggle.textContent = 'DARK MODE';
     }
 });
+
