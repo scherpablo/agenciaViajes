7e84123fa1d5c6700947f1a51c2e852b4c4a1165
scherpablo <scherpablo@gmail.com>
2023-05-10 13:11:32 -0300

    quinto commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ db.authenticate()
     .catch(error => console.log(error));
 
 //Definir Puerto
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;  
 
 //Habilitamos PUG
 app.set('view engine', 'pug');
